7ca1e23db37a4679c84c6d8f5e822f552d2d1073
Brian Picciano <brian.picciano@escapemg.com>
2013-03-04 16:19:25 -0500

    updated project deets

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clerb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "It's like ERB, but in clojure!"
+  :url "https://github.com/mediocregopher/clerb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
